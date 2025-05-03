21a645e99f496d54b42e57cfa0e5376f345a9d3e
SazaO_o <121901716+Saza-dev@users.noreply.github.com>
2025-05-03 16:50:33 +0530

    Streamlit app

---
+++ main.py
@@ -0,0 +1,54 @@
+# libraries
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model
+
+# Load the IMDB dataset and word index
+word_index = imdb.get_word_index()
+reverse_word_index = {value:key for key,value in word_index.items()}
+
+# load the model
+model = load_model('simple_rnn_imdb.h5')
+
+# function to decode the reviews 
+def decode_review (encoded_review):
+    return ' '.join([reverse_word_index.get(i-3,'?')for i in encoded_review])
+
+# function to preprocess user input
+def preprocess_text(text):
+    words = text.lower().split()
+    encoded_review = [word_index.get(word,2) + 3 for word in words]
+    padded_review = sequence.pad_sequences([encoded_review],maxlen=500)
+    return padded_review
+
+# Prediction function
+def predict_sentiment(review):
+    preprocessed_input= preprocess_text(review)
+    prediction = model.predict(preprocessed_input)
+
+    sentiment = 'Positive' if prediction[0][0] > 0.5 else 'Negative'
+
+    return sentiment, prediction[0][0]
+
+
+import streamlit as st
+
+# streamlit app
+st.title("IMDB Movie Review Sentiment Analysiis")
+st.write("Enter a movie review to classify it as positive or negative")
+
+# user input
+user_input = st.text_area('Movie Review')
+
+if st.button('Classify'):
+    ## making the prediction
+    sentiment,score = predict_sentiment(user_input)
+
+    # display the result
+    st.write(f"Sentiment:{sentiment}")
+    st.write(f"Prediction score: {score}")
+
+else:
+    st.write ('Please enter a movie review.')
